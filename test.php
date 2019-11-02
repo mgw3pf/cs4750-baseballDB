4f3eb34470aa58c5337a877b4a54a2771fb6d5c2
Mike Wood <mgw3pf@virginia.edu>
2019-11-01 22:01:38 -0400

    add-on to last commit

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 
-include_once("library.php")
+$SERVER = 'cs4750.cs.virginia.edu';
+$USERNAME = 'reg3dq';
+$PASSWORD = 'Databases2019';
+$DATABASE = 'reg3dq';
+//include_once("library.php")
 $firstname = filter_input(INPUT_POST, 'firstname')
 if (!empty($firstname)) {
     // Create Connection
