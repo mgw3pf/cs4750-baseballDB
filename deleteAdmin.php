93b56759aa2422e576553488e81232ea78faa154
Robyn Guarriello <reg3dq@virginia.edu>
2019-11-08 12:30:50 -0500

    fixed security bugs in creating/deleting admins

--- deleteAdmin.php
+++ deleteAdmin.php
@@ -4,11 +4,15 @@ if(!(isset($_SESSION['login']) && $_SESSION['login']!='')){
         header("Location: login.php");
 }
 $username = $_GET['id'];
+$currentUser = $_SESSION['username'];
 $SERVER = 'cs4750.cs.virginia.edu';
 $USERNAME = 'reg3dq';
 $PASSWORD = 'Databases2019';
 $DATABASE = 'reg3dq';
 $conn = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+$sql = "SELECT * FROM user_role NATURAL JOIN roles NATURAL JOIN role_perm NATURAL JOIN permissions WHERE username = '$currentUser' AND perm_id = '3'";
+$result = $conn->query($sql);   
+if ($result->num_rows > 0){
   if (mysqli_connect_error()) {
       die('Connect Error ('. mysqli_connect_errno() .')'. mysqli_connect_error());
   } else {
@@ -21,6 +25,10 @@ $conn = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
         else{
                 echo "Error updating admin permissions";
         }
+}
+}
+else{
+        header("Location: index.php");
 }
 
 ?>
