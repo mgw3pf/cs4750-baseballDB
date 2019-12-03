8016c9fe5c9ad7602c591dcc7f64693e3c46a293
Mike Wood <mgw3pf@virginia.edu>
2019-12-03 14:46:37 -0500

    display changes pt 2

--- player.php
+++ player.php
@@ -18,6 +18,11 @@ $player = $_GET['id'];
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
+
+table, th, td {
+  border: 1px solid white;
+}
+
 body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
 .w3-row-padding img {margin-bottom: 12px}
 /* Set the width of the sidebar to 120px */
